c7f9fa04b58fac5a039a7b63279a0cf28a01f73d
SlightlySpicyRabbit <haoyu.xia9903@gmail.com>
2024-04-22 21:34:04 +0800

    update annotation of 'Trainer'

--- method/Trainer.py
+++ method/Trainer.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn, optim
+from torch import nn
 from torch.utils.data import DataLoader
 
 
@@ -8,47 +8,36 @@ class Trainer:
     Train a deep neural network and see how it works on a validation set during training
     """
 
-    def __init__(
-        self,
-        model,
-        batch_size: int,
-        num_epoch: int,
-        training_set,
-        validation_set=False,
-        verify_step_size=10,
-    ):
+    def __init__(self, model, batch_size: int, num_epoch: int, training_set, validation_set=None, verify_step_size=10):
         """
-            Initialization Trainer. Criterion defaults to nn.CrossEntropyLoss()
-        Args:
-            model (_type_): Pytorch neural network
-            batch_size (int): How many data are extracted for training/validation at a time
-            num_epoch (int): How many times will the training_set be iteratively trained
-            training_set (TensorDataset): Data set for training
-            validation_set (TensorDataset, optional): Data set for validation. Defaults to False mean does not compute the model's loss value on the validation set during training.
-            verify_step_size (int, optional): How many epochs to calculate the loss value of model on validation_set. Defaults to 10. This parameter is invalid when validation_set is False.
+        Initialization Trainer. Criterion defaults to nn.CrossEntropyLoss()
+        :param model: Pytorch neural network
+        :param batch_size: How many data are extracted for training/validation at a time
+        :param num_epoch: How many times will the training_set be iteratively trained
+        :param training_set: Data set for training
+        :param validation_set: Data set for validation. (optional) Defaults to None mean does not compute the model's loss value on the validation set during training.
+        :param verify_step_size: How many epochs to calculate the loss value of model on validation_set. (optional) Defaults to 10. (This parameter is invalid when validation_set is False)
         """
+        # Initialize the global variable
         self.model = model
         self.num_epoch = num_epoch
-        self.training_loader = DataLoader(
-            training_set, batch_size=batch_size, shuffle=True
-        )
-        if validation_set is False:
-            self.validation_loader = False
-        else:
-            self.validation_loader = DataLoader(
-                validation_set, batch_size=batch_size, shuffle=True
-            )
         self.verify_step_size = verify_step_size
+        # Use torch's DataLoader to load the data
+        self.training_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True, drop_last=True)
+        if validation_set is None:
+            self.validation_loader = None
+        else:
+            self.validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True, drop_last=True)
+        # Set criterion of loss function
         self.criterion = nn.CrossEntropyLoss()
 
     def __calculate_loss(self, data: torch.Tensor) -> torch.Tensor:
         """
         Private function to calculate the loss
-        :param data: Data required to calculate loss value
-        :param criterion: Criteria for calculation of losses
-        :return: loss value of data
+        :param data: Data required to calculate loss
+        :return: loss of data
         """
-        # Reshape the data
+        # Extract label and input
         img, label = data
         # Acceleration with cuda
         if torch.cuda.is_available():
@@ -67,154 +56,95 @@ class Trainer:
         """
         Private function to training model
         :param optimizer: Optimizer based on Pytorch
-        :param criterion: Scoring criteria based on Pytorch
-        :param train_loader: Training dataset
-        :param train_loader_for_loss: The dataset used to calculate the loss
-        :return: A list consisting of all the loss values from the training process
+        :return: A tuple with 4 list. It records the loss value and index of training/validation set
         """
         # Initialize
-        loss_list_index_training = []
-        loss_list_value_training = []
-        loss_list_index_validation = []
-        loss_list_value_validation = []
-        # Training
+        loss_list_index_training, loss_list_value_training = [], []
+        loss_list_index_validation, loss_list_value_validation = [], []
+        # Iterative for epoch
         for epoch in range(self.num_epoch):
+            # Training in each epoch
             for index, data in enumerate(self.training_loader):
+                # Initialize
                 loss_list = []
+                # Calculate loss
                 loss = self.__calculate_loss(data)
+                # Update parameter
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+                # Keep track of loss value
                 loss_list.append(loss.data.item())
                 print(f"epoch: {epoch} steps: {index} loss: {loss.data.item():.4f}")
+            # Keep track of loss value
             loss_list_index_training.append(epoch)
             loss_list_value_training.append(sum(loss_list) / len(loss_list))
-            if epoch % self.verify_step_size == 0:
+            # Validate as required
+            if self.validation_loader is not None and epoch % self.verify_step_size == 0:
                 with torch.no_grad():
+                    # validation in each epoch
                     for index, data in enumerate(self.validation_loader):
+                        # Initialize
                         loss_list = []
+                        # calculate loss
                         loss = self.__calculate_loss(data)
-                        # Keep track of solutions
+                        # Keep track of loss value
                         loss_list.append(loss.data.item())
+                # Keep track of loss value
                 loss_list_index_validation.append(epoch)
                 loss_list_value_validation.append(sum(loss_list) / len(loss_list))
-                print(
-                    f"validation_set epoch: {epoch} loss: {sum(loss_list)/len(loss_list):.4f}"
-                )
+                print(f"validation_set epoch: {epoch} loss: {sum(loss_list)/len(loss_list):.4f}")
         # Return
-        return (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        )
+        return loss_list_index_training, loss_list_value_training, loss_list_index_validation, loss_list_value_validation
 
-    def sgd(self, learning_rate: float) -> list:
+    def sgd(self, learning_rate: float) -> tuple[list, list, list, list]:
         """
         Training a model by using SGD
         :param learning_rate: Learning rate of training
-        :return: A list consisting of all the loss values from the training process
+        :return: A tuple with 4 list. It records the loss value and index of training/validation set (Used to track the training process and does NOT affect the training effect)
         """
-        # Define criterion and optimizer
-        optimizer = optim.SGD(self.model.parameters(), lr=learning_rate)
+        # Initialize optimizer
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
         # Training
         print("Training by SGD")
-        (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        ) = self.__train_model(optimizer)
-        # Return
-        return (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        )
+        return self.__train_model(optimizer)
 
-    def momentum(self, learning_rate: float, beta_1: float) -> list:
+    def momentum(self, learning_rate: float, beta_1: float) -> tuple[list, list, list, list]:
         """
         Training a model by using momentum
-        :param dataset: Total training set
         :param learning_rate: Learning rate of training
         :param beta_1: Factor for first-order moment estimation
-        :return: A list consisting of all the loss values from the training process
+        :return: A tuple with 4 list. It records the loss value and index of training/validation set (Used to track the training process and does NOT affect the training effect)
         """
-        # Define criterion and optimizer
-        optimizer = optim.SGD(
-            self.model.parameters(),
-            lr=learning_rate,
-            momentum=beta_1,
-            dampening=(1 - beta_1),
-        )
+        # Initialize optimizer
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate, momentum=beta_1, dampening=(1 - beta_1))
         # Training
         print("Training by momentum")
-        (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        ) = self.__train_model(optimizer)
-        # Return
-        return (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        )
+        return self.__train_model(optimizer)
 
-    def rmsprop(self, dataset, learning_rate: float, beta_2: float) -> list:
+    def rmsprop(self, learning_rate: float, beta_2: float) -> tuple[list, list, list, list]:
         """
         Training a model by using RMSprop
-        :param dataset: Total training set
         :param learning_rate: Learning rate of training
         :param beta_2: Factor for Second-order moment estimation
-        :return: A list consisting of all the loss values from the training process
+        :return: A tuple with 4 list. It records the loss value and index of training/validation set (Used to track the training process and does NOT affect the training effect)
         """
-        optimizer = optim.RMSprop(
-            self.model.parameters(), lr=learning_rate, alpha=beta_2, centered=True
-        )
+        # Initialize optimizer
+        optimizer = torch.optim.RMSprop(self.model.parameters(), lr=learning_rate, alpha=beta_2, centered=True)
         # Training
         print("Training by RMSprop")
-        (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        ) = self.__train_model(optimizer)
-        # Return
-        return (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        )
+        return self.__train_model(optimizer)
 
-    def adam(self, dataset, learning_rate: float, beta_1: float, beta_2: float) -> list:
+    def adam(self, dataset, learning_rate: float, beta_1: float, beta_2: float) -> tuple[list, list, list, list]:
         """
         Training a model by using adam
-        :param dataset: Total training set
         :param learning_rate: Learning rate of training
         :param beta_1: Factor for first-order moment estimation
         :param beta_2: Factor for Second-order moment estimation
-        :return: A list consisting of all the loss values from the training process
+        :return: A tuple with 4 list. It records the loss value and index of training/validation set (Used to track the training process and does NOT affect the training effect)
         """
-        optimizer = optim.Adam(
-            self.model.parameters(), lr=learning_rate, betas=(beta_1, beta_2)
-        )
+        # Initialize optimizer
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
         # Training
         print("Training by adam")
-        (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        ) = self.__train_model(optimizer)
-        # Return
-        return (
-            loss_list_index_training,
-            loss_list_value_training,
-            loss_list_index_validation,
-            loss_list_value_validation,
-        )
+        return self.__train_model(optimizer)
